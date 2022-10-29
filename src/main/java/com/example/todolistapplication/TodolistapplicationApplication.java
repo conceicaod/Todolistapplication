dacd08b489a9083ff4b6d27e7d47bd210c836c91
conceicaod <60795870+conceicaod@users.noreply.github.com>
2022-10-29 14:04:29 -0400

    Initial commit

---
+++ src/main/java/com/example/todolistapplication/TodolistapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistapplicationApplication.class, args);
+    }
+
+}
